eaf64f6fca6fd4d6da53a70273da127eff9ac740
Yazan Obeidi <yazan@protonmail.ch>
2018-03-02 19:44:33 -0500

    add config, add view methods

--- DeepFlow/viewer.py
+++ DeepFlow/viewer.py
@@ -4,5 +4,23 @@ class View(object):
     def __init__(self):
         print('Iitializing viewer')
 
-    def test(self):
-        print('test')
+    def load_data(self):
+        pass
+
+    def load_header(self):
+        pass
+
+    def table(self):
+        pass
+
+    def plot(self):
+        pass
+
+    def histogram(self):
+        pass
+
+    def summary_statistics(self):
+        pass
+
+    def clusters(self):
+        pass
